7e2293efa2895c6a25f46f6f77edcb286030463b
Qizhe <qizhe.cai@gmail.com>
2018-12-02 23:40:36 -0500

    add runscript

--- py/bandwidth/run_script.sh
+++ py/bandwidth/run_script.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-loads=(5 6 7 8 9 10)
-algos=(pfabric phost fastpass ranking)
+bandwidths=(5 10 15 20 25 30 35 40 45 50)
+algos=(fastpass phost pfabric ranking)
 calc(){ awk "BEGIN { print "$*" }"; }
 pids=()
 
-OUTPUT_FOLDER=../result/load
+OUTPUT_FOLDER=../result/bandwidth
 DATE=$1
 TRACE=$2
 mkdir $OUTPUT_FOLDER
 mkdir $OUTPUT_FOLDER/"$DATE"
 for i in ${!algos[*]}
 do 
-	for index in ${!loads[*]};
+	for index in ${!bandwidths[*]};
 	do
-    	load=${loads[$index]}
+    	bandwidth=${bandwidths[$index]}
 	    algo=${algos[$i]}
 	    # echo conf_"$algo"_dctcp_$load.txt
 	    # echo "$OUTPUT_FOLDER"/result_"$algo"_dctcp_"$load".txt
-	    ../../simulator 1 conf_"$algo"_"$2"_"$load".txt > "$OUTPUT_FOLDER/$DATE"/result_"$algo"_"$2"_"$load".txt&
+	    ../../simulator 1 conf_"$algo"_"$2"_"$bandwidth".txt > "$OUTPUT_FOLDER/$DATE"/result_"$algo"_"$2"_"$bandwidth".txt&
 	#	nohup ./batch_simulate_sflow.py -P $p -F ../../../data/ -t ${threshold[$index]} -i 10 -N 1000 -s 1 -l results/conext18/flows/percentage-${percentage[$index]}.log &
 	    pids[${index}]=$!
 	done
