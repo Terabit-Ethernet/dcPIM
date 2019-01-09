aa4f107854843e295f026394f8ba836de4f97daf
Qizhe <qizhe.cai@gmail.com>
2019-01-09 14:08:56 -0500

    change bandwidth

--- py/bandwidth/bandwidth.py
+++ py/bandwidth/bandwidth.py
@@ -8,7 +8,7 @@ propagation_delay: 0.0000002
 bandwidth: {3}
 queue_type: 2
 flow_type: 2
-num_flow: 200000
+num_flow: 1000000
 flow_trace: ../CDF_{1}.txt
 cut_through: 0
 mean_flow_size: 0
@@ -53,7 +53,7 @@ propagation_delay: 0.0000002
 bandwidth: {2}
 queue_type: 2
 flow_type: 112
-num_flow: 200000
+num_flow: 1000000
 flow_trace: ../CDF_{1}.txt
 cut_through: 0
 mean_flow_size: 0
@@ -98,7 +98,7 @@ propagation_delay: 0.0000002
 bandwidth: {2}
 queue_type: 2
 flow_type: 114
-num_flow: 200000
+num_flow: 1000000
 flow_trace: ../CDF_{1}.txt
 cut_through: 0
 mean_flow_size: 0
@@ -143,7 +143,7 @@ propagation_delay: 0.0000002
 bandwidth: {2}
 queue_type: 2
 flow_type: 112
-num_flow: 200000
+num_flow: 1000000
 flow_trace: ../CDF_{1}.txt
 cut_through: 0
 mean_flow_size: 0
@@ -188,7 +188,7 @@ propagation_delay: 0.0000002
 bandwidth: {1}
 queue_type: 2
 flow_type: 115
-num_flow: 200000
+num_flow: 1000000
 flow_trace: ../CDF_{0}.txt
 cut_through: 0
 mean_flow_size: 0
@@ -210,10 +210,10 @@ token_timeout: 2
 token_resend_timeout: 1
 token_window: 1
 token_window_timeout: 1.1
-rankinghost_idle_timeout: 0.5
-ranking_reset_epoch: 5
-ranking_max_tokens: 2
-ranking_controller_epoch: 0.25
+rankinghost_idle_timeout: 5.5
+ranking_max_tokens: 10
+ranking_min_tokens: 5
+ranking_controller_epoch: 5
 ddc: 0
 ddc_cpu_ratio: 0.33
 ddc_mem_ratio: 0.33
