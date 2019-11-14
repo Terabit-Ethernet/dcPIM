c9bdf81b0b43d3286a8aa08f23849396f79609d1
qizhe <qizhe.cai@gmail.com>
2019-11-13 21:46:33 -0500

    add change

--- py/oversubscribe/run_script_ruf.sh
+++ py/oversubscribe/run_script_ruf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-algos=(ruf_local)
-local=(0 1)
-conn=(0 1)
+algos=(ruf)
+locals=(0 1)
+conns=(0 1)
 aids=()
 OUTPUT_FOLDER=../result/oversubscribe
 DATE=$1
@@ -11,14 +11,16 @@ mkdir $OUTPUT_FOLDER
 mkdir $OUTPUT_FOLDER/"$DATE"
 for i in ${!algos[*]}
 do 
-	for j in ${!local[*]}
+	for j in ${!locals[*]}
 	do
-		for k in ${!conn[*]}
+		for k in ${!conns[*]}
 		do 
 			algo=${algos[$i]}
-			    # echo conf_"$algo"_dctcp_$incast.txt
+			loco=${locals[$j]}
+  			con=${conns[$k]}
+			  # echo conf_"$algo"_dctcp_$incast.txt
 			    # echo "$OUTPUT_FOLDER"/result_"$algo"_dctcp_"$incast".txt
-			../../simulator 1 conf_"$algo"_"$2"_"$j"_"$k".txt > "$OUTPUT_FOLDER/$DATE"/result_"$algo"_"$2"_"$j"_"$k".txt&
+			../../simulator 1 conf_"$algo"_"$2"_"$loco"_"$con".txt > "$OUTPUT_FOLDER/$DATE"/result_"$algo"_"$2"_"$loco"_"$con".txt&
 			 pids[${i}]=$!
 		#	for pid in ${pids[*]}; 
 		#	do
