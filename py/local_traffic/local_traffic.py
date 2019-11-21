76d470ec7a5447cd1c89b1e2a84c88f69fa922aa
Qizhe <qizhe.cai@gmail.com>
2019-11-21 14:39:04 -0500

    add change

--- py/local_traffic/local_traffic.py
+++ py/local_traffic/local_traffic.py
@@ -193,6 +193,7 @@ queue_type: 2
 flow_type: 115
 os_ratio: {4}
 num_hosts: {5}
+local_flow_precentage: {0}
 num_flow: 1000000
 flow_trace: ../CDF_{1}.txt
 cut_through: 0
@@ -202,7 +203,7 @@ preemptive_queue: 0
 big_switch: 0
 host_type: 15
 traffic_imbalance: 0
-load: {0}
+load: 0.3
 reauth_limit: 3
 magic_trans_slack: 1.1
 magic_delay_scheduling: 1
@@ -287,7 +288,8 @@ num_host_types: 13
 #runs = ['pfabric', 'phost', 'fastpass', 'ruf', 'pim']
 runs = ['ruf']
 workloads = ['aditya', 'dctcp', 'datamining', 'constant']
-loads = [  0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.40, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.50, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.60, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69, 0.70, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.80, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89]
+#precentage = [  0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.40, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.50, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.60, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69, 0.70, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.80, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89]
+precentage= [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 for r in runs:
     for w in workloads:
         #  generate conf file
@@ -295,7 +297,7 @@ for r in runs:
             if r == "ruf":
                 for i in range(2):
                     for j in range(2):
-                        conf_str = conf_str_ruf.format(load, w, i, ji, 0.2, 144 / 0.2)
+                        conf_str = conf_str_ruf.format(precentage, w, i, j, 0.2, 144 / 0.2)
                         confFile = "conf_{0}_{1}_{2}_{3}_{4}.txt".format(r, w, i, j, str("{:.2f}".format(load)))
                         with open(confFile, 'w') as f:
                             f.write(conf_str)
