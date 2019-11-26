a0225c62f774d8532da174dcdf6bab03e1584a22
Qizhe <qc228@cornell.edu>
2019-11-26 16:50:43 -0500

    add change

--- py/local_traffic/run.py
+++ py/local_traffic/run.py
@@ -6,7 +6,7 @@ import subprocess
 runs = ['ruf']
 workloads = ['aditya']
 #precentage = [  0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.40, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.50, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.60, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69, 0.70, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.80, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89]
-over_subscription = [1, 2, 4, 8, 16, 32]
+over_subscription = [1, 2, 4, 8, 16]
 OUTPUT_FOLDER = "../result/local_traffic"
 DATE = sys.argv[1]
 
@@ -26,9 +26,12 @@ def safe_open_w(path):
     return open(path, 'w')
 
 for r in runs:
+
     for w in workloads:
         #  generate conf file
+
         for o in over_subscription:
+            pros = []
             os_ratio = 1.0 / o
             load = 0.0
             first_time = False
@@ -39,7 +42,8 @@ for r in runs:
                             confFile = "conf_{0}_{1}_{2}_{3}_{4}_{5}.txt".format(r, w, i, j, o, load)
                             resultFile = "{0}/{1}/result_{2}_{3}_{4}_{5}_{6}_{7}.txt".format(OUTPUT_FOLDER,DATE, r, w, i, j, o, load)
                             f = safe_open_w(resultFile)
-                            subprocess.Popen(["../../simulator", "1", confFile],stdout=f)
+                            p = subprocess.Popen(["../../simulator", "1", confFile],stdout=f)
+                            pros.append(p)
                             f.close()
                 else:
                     if r == 'pfabric':
@@ -61,3 +65,6 @@ for r in runs:
                 if first_time == False and load > 1.0 - os_ratio:
                     load = 1.0 - os_ratio
                     first_time = True
+        for p in pros:
+            print "process"
+            p.wait()
