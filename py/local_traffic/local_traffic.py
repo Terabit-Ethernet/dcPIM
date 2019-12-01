f18e2bda3e27e404c3bbab5fa582a611abdd3e54
Qizhe <qc228@cornell.edu>
2019-12-01 09:07:36 -0500

    add change

--- py/local_traffic/local_traffic.py
+++ py/local_traffic/local_traffic.py
@@ -6,7 +6,8 @@ propagation_delay: 0.0000002
 bandwidth: 10000000000.0
 queue_type: 2
 flow_type: 2
-os_ratio: 0.5
+os_ratio: {2}
+num_hosts: {3}
 num_flow: 1000000
 flow_trace: ../CDF_{1}.txt
 cut_through: 0
@@ -16,7 +17,7 @@ preemptive_queue: 0
 big_switch: 0
 host_type: 1
 traffic_imbalance: 0
-load: {0}
+local_load: {0}
 reauth_limit: 3
 magic_trans_slack: 1.1
 magic_delay_scheduling: 1
@@ -53,7 +54,8 @@ propagation_delay: 0.0000002
 bandwidth: 10000000000.0
 queue_type: 2
 flow_type: 112
-os_ratio: 0.5
+os_ratio: {2}
+num_hosts: {3}
 num_flow: 1000000
 flow_trace: ../CDF_{1}.txt
 cut_through: 0
@@ -63,7 +65,7 @@ preemptive_queue: 0
 big_switch: 0
 host_type: 12
 traffic_imbalance: 0
-load: {0}
+local_load: {0}
 reauth_limit: 3
 magic_trans_slack: 1.1
 magic_delay_scheduling: 1
@@ -99,7 +101,8 @@ propagation_delay: 0.0000002
 bandwidth: 10000000000.0
 queue_type: 2
 flow_type: 114
-os_ratio: 0.5
+os_ratio: {2}
+num_hosts: {3}
 num_flow: 1000000
 flow_trace: ../CDF_{1}.txt
 cut_through: 0
@@ -109,7 +112,7 @@ preemptive_queue: 0
 big_switch: 0
 host_type: 14
 traffic_imbalance: 0
-load: {0}
+local_load: {0}
 reauth_limit: 3
 magic_trans_slack: 1.1
 magic_delay_scheduling: 1
@@ -145,7 +148,8 @@ propagation_delay: 0.0000002
 bandwidth: 10000000000.0
 queue_type: 2
 flow_type: 112
-os_ratio: 0.5
+os_ratio: {2}
+num_hosts: {3}
 num_flow: 1000000
 flow_trace: ../CDF_{1}.txt
 cut_through: 0
@@ -155,7 +159,7 @@ preemptive_queue: 0
 big_switch: 0
 host_type: 16
 traffic_imbalance: 0
-load: {0}
+local_load: {0}
 reauth_limit: 3
 magic_trans_slack: 1.1
 magic_delay_scheduling: 1
@@ -243,7 +247,8 @@ propagation_delay: 0.0000002
 bandwidth: 10000000000.0
 queue_type: 2
 flow_type: 116
-os_ratio: 0.5
+os_ratio: {2}
+num_hosts: {3}
 num_flow: 1000000
 flow_trace: ../CDF_{1}.txt
 cut_through: 0
@@ -253,7 +258,7 @@ preemptive_queue: 0
 big_switch: 0
 host_type: 17
 traffic_imbalance: 0
-load: {0}
+local_load: {0}
 reauth_limit: 3
 magic_trans_slack: 1.1
 magic_delay_scheduling: 1
@@ -285,7 +290,7 @@ num_host_types: 13
 
 
 #runs = ['pfabric', 'phost', 'fastpass', 'ruf', 'pim']
-runs = ['ruf']
+runs = ['ruf', 'pfabric', 'phost', 'fastpass']
 workloads = ['aditya', 'dctcp', 'datamining']
 #precentage = [  0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.40, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.50, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.60, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69, 0.70, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.80, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89]
 over_subscription = [1, 2, 4, 8, 16]
@@ -306,17 +311,17 @@ for r in runs:
                                 f.write(conf_str)
                 else:
                     if r == 'pfabric':
-                        conf_str = conf_str_pfabric.format(load, w)
+                        conf_str = conf_str_pfabric.format(load, w, os_ratio, int(144 / os_ratio))
                     elif r == 'phost':
-                        conf_str = conf_str_phost.format(load, w)
+                        conf_str = conf_str_phost.format(load, w, os_ratio, int(144 / os_ratio))
                     elif r == 'fastpass':
-                        conf_str = conf_str_fastpass.format(load, w)
+                        conf_str = conf_str_fastpass.format(load, w, os_ratio, int(144 / os_ratio))
                     elif r == 'random':
-                        conf_str = conf_str_random.format(load, w)
+                        conf_str = conf_str_random.format(load, w, os_ratio, int(144 / os_ratio))
 
                     elif r == 'pim':
-                        conf_str = conf_str_pim.format(load, w)
-                    confFile = "conf_{0}_{1}_{2}.txt".format(r, w, str("{:.2f}".format(load)))
+                        conf_str = conf_str_pim.format(load, w, os_ratio, int(144 / os_ratio))
+                    confFile = "conf_{0}_{1}_{2}_{3}.txt".format(r, w, o, load)
                     with open(confFile, 'w') as f:
                         print confFile
                         f.write(conf_str)
--- py/local_traffic/run.py
+++ py/local_traffic/run.py
@@ -3,7 +3,7 @@ import errno
 import sys
 import subprocess
 
-runs = ['ruf']
+runs = ['ruf', 'pfabric', 'fastpass', 'phost']
 workloads = ['aditya']
 #precentage = [  0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.40, 0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.50, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.60, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69, 0.70, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.80, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89]
 over_subscription = [1, 2, 4, 8, 16]
@@ -40,7 +40,7 @@ for r in runs:
                     for i in range(2):
                         for j in range(2):
                             confFile = "conf_{0}_{1}_{2}_{3}_{4}_{5}.txt".format(r, w, i, j, o, load)
-                            resultFile = "{0}/{1}/result_{2}_{3}_{4}_{5}_{6}_{7}.txt".format(OUTPUT_FOLDER,DATE, r, w, i, j, o, load)
+                            resultFile = "{0}/{1}/result_{2}_{3}_{4}_{5}_{6}_{7}.txt".format(OUTPUT_FOLDER,DATE, r, i, j, w, o, load)
                             f = safe_open_w(resultFile)
                             p = subprocess.Popen(["../../simulator", "1", confFile],stdout=f)
                             pros.append(p)
