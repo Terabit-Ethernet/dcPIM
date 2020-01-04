f260ba97ed426fde6f90855794375ac727e98a52
Qizhe <qc228@cornell.edu>
2020-01-03 22:10:24 -0500

    add change

--- py/max_load/run_script.sh
+++ py/max_load/run_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-loads=(0 1 2 3 4 5 6 7 8 9)
-algos=(pim phost pfabric fastpass)
+loads=(2 4 6 8)
+algos=(pim)
 calc(){ awk "BEGIN { print "$*" }"; }
 pids=()
 
@@ -19,8 +19,11 @@ do
 	    # echo conf_"$algo"_dctcp_$load.txt
 	    # echo "$OUTPUT_FOLDER"/result_"$algo"_dctcp_"$load".txt
 	    #../../simulator 1 conf_"$algo"_"$2"_"0.7$load".txt > "$OUTPUT_FOLDER/$DATE"/result_"$algo"_"$2"_"0.7$load".txt&
-	    ../../simulator 1 conf_"$algo"_"$2"_"0.8$load".txt > "$OUTPUT_FOLDER/$DATE"/result_"$algo"_"$2"_"0.8$load".txt&
-	#	nohup ./batch_simulate_sflow.py -P $p -F ../../../data/ -t ${threshold[$index]} -i 10 -N 1000 -s 1 -l results/conext18/flows/percentage-${percentage[$index]}.log &
+	    #../../simulator 1 conf_"$algo"_"$2"_"0.8$load".txt > "$OUTPUT_FOLDER/$DATE"/result_"$algo"_"$2"_"0.8$load".txt&
+	    ../../simulator 1 conf_"$algo"_"$2"_"0.8$load".txt > "$OUTPUT_FOLDER/$DATE"/trace_"$2"_"0.8$load".txt&
+            #../../simulator 2 conf_"$algo"_"$2"_"0.5$load".txt > "$OUTPUT_FOLDER/$DATE"/trace_"$2"_"0.5$load".txt&
+
+	    #	nohup ./batch_simulate_sflow.py -P $p -F ../../../data/ -t ${threshold[$index]} -i 10 -N 1000 -s 1 -l results/conext18/flows/percentage-${percentage[$index]}.log &
 	    pids[${index}]=$!
 	done
 	for pid in ${pids[*]}; 
