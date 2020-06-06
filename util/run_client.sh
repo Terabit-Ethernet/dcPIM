fb43243b166f01f196f4c5f7345d573bf90d489b
Qizhe Cai <qizhe.cai@gmail.com>
2020-06-06 11:51:03 -0400

    add client script

--- util/run_client.sh
+++ util/run_client.sh
@@ -1,6 +1,7 @@
 CORES=$1
+score=$2
 core_id=0
 while (( core_id < CORES ));do
-	taskset -c $core_id ./homa_test 192.168.10.115:6000 --sp $(( 4000 + core_id )) --count 40 dcacpping &
+	taskset -c $core_id ./homa_test 192.168.10.115:$((4000+score)) --sp $(( 4000 + core_id )) --count 40 dcacpping &
         (( core_id++ ))
 done
