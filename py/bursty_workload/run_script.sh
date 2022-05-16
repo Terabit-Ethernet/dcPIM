cd223af3345d4bd50533f729c4346ce717b7c7d2
qizhe <qizhe.cai@gmail.com>
2022-05-16 11:56:58 -0400

    add change for bursty workload

--- py/bursty_workload/run_script.sh
+++ py/bursty_workload/run_script.sh
@@ -18,7 +18,7 @@ do
 	    algo=${algos[$i]}
 	    # echo conf_"$algo"_dctcp_$load.txt
 	    # echo "$OUTPUT_FOLDER"/result_"$algo"_dctcp_"$load".txt
-	    ../../simulator 1 conf_"$algo"_"$2"_"$load".txt > "$OUTPUT_FOLDER/$DATE"/result_"$algo"_"$2"_"$load".txt&
+	    ../../simulator 1 conf_"$algo"_"$TRACE"_"$load".txt > "$OUTPUT_FOLDER/$DATE"/result_"$algo"_"$TRACE"_"$load".txt&
 	#	nohup ./batch_simulate_sflow.py -P $p -F ../../../data/ -t ${threshold[$index]} -i 10 -N 1000 -s 1 -l results/conext18/flows/percentage-${percentage[$index]}.log &
 	    pids[${index}]=$!
 	done
