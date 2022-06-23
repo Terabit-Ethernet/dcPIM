0cde82cab2767406177d6504645c7707d7064368
qizhe cai <qizhe.cai@gmail.com>
2022-06-23 14:00:55 -0400

    Update run.sh

--- simulator/py/bursty_workload/run.sh
+++ simulator/py/bursty_workload/run.sh
@@ -2,5 +2,5 @@
 
 python bursty.py
 ./run_script.sh 5.15 imc10 &
-./run_script.sh 5.15 websearch &
-./run_script.sh 5.15 datamining & 
+#./run_script.sh 5.15 websearch &
+#./run_script.sh 5.15 datamining & 
