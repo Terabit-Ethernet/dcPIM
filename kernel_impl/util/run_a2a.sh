46547acd298e4ca94256b904a014c06dbea4a757
qizhe <qizhe.cai@gmail.com>
2023-06-10 18:39:42 +0000

    add script

--- kernel_impl/util/run_a2a.sh
+++ kernel_impl/util/run_a2a.sh
@@ -2,6 +2,6 @@ TASKSET="0,4,8,12,16,20,24,28,32,36,40,44,48,52,56"
 server=0
 NSERVER=15
 while (( server < NSERVER ));do
-        sudo taskset -c $TASKSET /home/qizhe/dcpim_kernel/util/server --ip 192.168.11.125 --port $((10000 + server)) --pin > server_$((server)).log &
+        sudo taskset -c $TASKSET /home/qizhe/dcPIM/kernel_impl/util/server --ip 192.168.11.125 --port $((10000 + server)) --pin > server_$((server)).log &
         (( server++ ))
 done
