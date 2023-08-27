ef56ddda9b96afa9115c39b5b8818d767cecc912
caiqizhe <caiqizhe@localhost>
2023-08-27 17:39:34 -0600

    add script for cloudlab

---
+++ kernel_impl/cloudlab_script/run_client.sh
@@ -0,0 +1,19 @@
+NCLIENT=$1
+SYS=$2
+serverip=$3
+flow=0
+core=0
+NSERVER=15
+TASKSET="0,4,8,12,16,20,24,28,32,36,40,44,48,52,56"
+echo "NUM client: $NCLIENT"
+while (( flow < NCLIENT ));do
+        dport=$((10000 + flow % NSERVER))
+        if [[ $SYS == "dcpim" ]]
+        then
+                        sudo LD_PRELOAD=/home/qizhe/dcPIM/kernel_impl/custom_socket/socket_wrapper.so taskset -c $((core)) iperf3 -c $serverip -p $((dport)) --cport $((dport)) -t 120 -4 > client_$((flow)).log &
+        else
+                        sudo taskset -c $((core)) iperf3 -c $serverip -p $((dport)) --cport $((dport)) -t 120 -4 > client_$((flow)).log &
+        fi
+        ((core = (core + 4) % 64))
+        (( flow++ ))
+done
