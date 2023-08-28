dac1136cc60864643819b35bb4cf39aa1d488b12
caiqizhe <caiqizhe@localhost>
2023-08-27 20:30:25 -0600

    add script for cloudlab

--- kernel_impl/cloudlab_script/run_client.sh
+++ kernel_impl/cloudlab_script/run_client.sh
@@ -1,18 +1,19 @@
 NCLIENT=$1
 SYS=$2
-serverip=$3
+serverindex=$3
 flow=0
 core=0
 NSERVER=15
 TASKSET="0,4,8,12,16,20,24,28,32,36,40,44,48,52,56"
 echo "NUM client: $NCLIENT"
 while (( flow < NCLIENT ));do
-        dport=$((10000 + flow % NSERVER))
+        dport=$((4000 * serverindex + flow % NSERVER))
+        serverip="10.10.1.$serverindex"
         if [[ $SYS == "dcpim" ]]
         then
-                        sudo LD_PRELOAD=/home/qizhe/dcPIM/kernel_impl/custom_socket/socket_wrapper.so taskset -c $((core)) iperf -c $serverip -p $((dport)) -B $clientip:$dport  -t 120 -4 > client_$((flow)).log &
+                        sudo LD_PRELOAD=/home/qizhe/dcPIM/kernel_impl/custom_socket/socket_wrapper.so taskset -c $((core)) iperf3 -c $serverip -p $((dport)) --cport $dport  -t 120 -4  > client_"$flow".log &
         else
-                        sudo taskset -c $((core)) iperf -c $serverip -p $((dport)) -B $clientip:$dport -t 120 -4 > client_$((flow)).log &
+                        sudo taskset -c $((core)) iperf -c $serverip -p $((dport)) --cport $dport -t 120 -4 > client_$((flow)).log &
         fi
         ((core = (core + 4) % 64))
         (( flow++ ))
--- kernel_impl/cloudlab_script/run_server.sh
+++ kernel_impl/cloudlab_script/run_server.sh
@@ -1,15 +1,21 @@
 TASKSET="0,4,8,12,16,20,24,28,32,36,40,44,48,52,56"
 server=0
-NSERVER=15
-core=0
+numiperf=15
 SYS=$1
-while (( server < NSERVER ));do
+servers=$2
+while (( server < servers ));do
+	core=0
+	iperf=0
+	while (( iperf < numiperf ));do
+
         if [[ $SYS == "dcpim" ]]
         then
-                sudo LD_PRELOAD=~/dcPIM/kernel_impl/custom_socket/socket_wrapper.so taskset -c $((core)) iperf -s -p $((10000 + server))  > server_$((server)).log &
+                sudo LD_PRELOAD=~/dcPIM/kernel_impl/custom_socket/socket_wrapper.so taskset -c $((core)) iperf3 -s -p $((4000 * (server + 1) + iperf))  > server_"$server"_"$iperf".log &
         else
-                sudo taskset -c $((core)) iperf -s -p $((10000 + server))  > server_$((server)).log &
+                sudo taskset -c $((core)) iperf3 -s -p $((4000 * (server + 1) + iperf))  > server_"$server"_"$iperf".log &
         fi
         ((core = (core + 4) % 64))
-        (( server++))
+        (( iperf++ ))
+    done
+    (( server++ ))
 done
