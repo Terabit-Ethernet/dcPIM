d98c00a8d6f1a58a6aac5a7652a7da5d647496b6
caiqizhe <caiqizhe@localhost>
2023-08-27 21:16:40 -0600

    add script for client

--- kernel_impl/cloudlab_script/run_client.sh
+++ kernel_impl/cloudlab_script/run_client.sh
@@ -1,19 +1,20 @@
 NCLIENT=$1
 SYS=$2
 serverindex=$3
+clientindex=$4
 flow=0
 core=0
 NSERVER=15
 TASKSET="0,4,8,12,16,20,24,28,32,36,40,44,48,52,56"
 echo "NUM client: $NCLIENT"
 while (( flow < NCLIENT ));do
-        dport=$((4000 * serverindex + flow % NSERVER))
+        dport=$((4000 * clientindex + flow % NSERVER))
         serverip="10.10.1.$serverindex"
         if [[ $SYS == "dcpim" ]]
         then
-                        sudo LD_PRELOAD=/home/qizhe/dcPIM/kernel_impl/custom_socket/socket_wrapper.so taskset -c $((core)) iperf3 -c $serverip -p $((dport)) --cport $dport  -t 120 -4  > client_"$flow".log &
+                        sudo LD_PRELOAD=/home/qizhe/dcPIM/kernel_impl/custom_socket/socket_wrapper.so taskset -c $((core)) iperf3 -c $serverip -p $((dport)) --cport $dport  -t 120 -4  > client_"$serverindex"_"$flow".log &
         else
-                        sudo taskset -c $((core)) iperf3 -c $serverip -p $((dport)) --cport $dport -t 120 -4 > client_$((flow)).log &
+                        sudo taskset -c $((core)) iperf3 -c $serverip -p $((dport)) --cport $dport -t 120 -4 > client_"$serverindex"_$((flow)).log &
         fi
         ((core = (core + 4) % 64))
         (( flow++ ))
