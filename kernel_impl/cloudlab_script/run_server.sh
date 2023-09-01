a1ae5af067d34658f09b26c554f0cf3773719870
caiqizhe <caiqizhe@localhost>
2023-09-01 13:13:53 -0400

    add cloudlab script change

--- kernel_impl/cloudlab_script/run_server.sh
+++ kernel_impl/cloudlab_script/run_server.sh
@@ -1,4 +1,4 @@
-TASKSET="0,4,8,12,16,20,24,28,32,36,40,44,48,52,56"
+tASKSET="0,4,8,12,16,20,24,28,32,36,40,44,48,52,56"
 server=0
 numiperf=15
 SYS=$1
@@ -10,7 +10,7 @@ while (( server < servers ));do
 
         if [[ $SYS == "dcpim" ]]
         then
-                sudo LD_PRELOAD=~/dcPIM/kernel_impl/custom_socket/socket_wrapper.so taskset -c $((core)) iperf3 -s -p $((4000 * (server + 1) + iperf))  > server_"$server"_"$iperf".log &
+                sudo LD_PRELOAD=/home/caiqizhe/dcPIM/kernel_impl/custom_socket/socket_wrapper.so taskset -c $((core)) iperf3 -s -p $((4000 * (server + 1) + iperf))  > server_"$server"_"$iperf".log &
         else
                 sudo taskset -c $((core)) iperf3 -s -p $((4000 * (server + 1) + iperf))  > server_"$server"_"$iperf".log &
         fi
