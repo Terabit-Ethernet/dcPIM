4fd3dca50174ca48978f79bdc9841fe3ee8f9477
qizhe <qizhe.cai@gmail.com>
2023-09-09 16:15:32 +0000

    change ptp script

--- kernel_impl/ptp/ptp_install.sh
+++ kernel_impl/ptp/ptp_install.sh
@@ -2,8 +2,9 @@ sudo apt-get remove linuxptp
 git clone http://git.code.sf.net/p/linuxptp/code linuxptp
 cd linuxptp/
 make
-sudo apt-get install checkinstall
-sudo checkinstall
+sudo make install
+#sudo apt-get install checkinstall
+#sudo checkinstall
 
 # remove ptp package
 # dpkg -r linux_ptp
